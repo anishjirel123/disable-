424cc1f322f553f34d8585fd52fbabb6313fbae5
anishjirel <anishjirel123@gmail.com>
2022-09-24 17:17:07 +0545

    disable

---
+++ resources/views/site/home.blade.php
@@ -0,0 +1,316 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Disable people</title>
+ <link rel="stylesheet" href=" {{ asset('site/bootstrap/css/bootstrap.css') }}">
+  <link rel="stylesheet" href="{{ asset('site/fontawesome/css/all.css') }}">
+  <link rel="stylesheet" href="{{ asset('site/css/style.css') }}">
+</head>
+        <!--  navbar section starts here  -->
+   <section id="navbar">
+      <div class="container">
+        <nav class="navbar navbar-expand-lg">
+          <a class="navbar-brand" href="#"><img src="{{asset('site/image/22.jpg')}}" alt="" class="img-fluid">Disabled</a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <i class="fas fa-bars"></i>
+          </button>
+        
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav text-center ml-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="#">HOME</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">ABOUTS</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">SERVICES</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="">CARCER</a>
+              </li>           
+              <li class="nav-item">
+                <a class="nav-link" href="#">CONTACT</a>
+              </li>
+            </ul>
+            <form class="d-flex" role="search">
+              <!-- <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"> -->
+            <!-- <button class="btn" type="submit">Search</button> -->
+          </form>
+          </div>
+        </nav>
+      </div>
+   </section>
+<!-- navbar section end here  -->
+
+   <!--slide us section starts here  -->
+   
+   <section id="slide">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6">
+            <div class="slide-content">
+                <h1>NEVER GIVE UP</h1> <br>
+                <h2>GREAT THINGS TAKE TIME</h2><br>
+                <h3>BE PATIENT</h3>
+                <a href="#" class="btn">READ MORE <i class="fas fa-book"></i></a>
+                  <a href="#" class="bt">GALLERY <i class="fa-solid fa-photo-film"></i></a>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="slide-img">
+              <img src="{{asset('site/image/3.jpg')}}" alt="" class="img-fluid">
+            </div>
+          </div>
+        </div>
+      </div>
+      </div>  
+  </section>
+<!-- slide us section ends here -->
+
+
+   <!--about section starts here -->
+   <section id="about">
+      <div class="container-fluid">
+          <div class="row">
+            <div class="col-md-4">
+              <div class="about-box">
+              <img src="{{asset('site/image/5.jpg')}}" alt="" class="img-fluid">
+
+              </div>      
+            </div>
+            <div class="col-md-4">
+              <div class="about-box">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit amet illo quae illum culpa facilis fuga, voluptatum tenetur voluptate architecto optio ex minima autem saepe harum voluptatem adipisci consequatur quaerat!</p>
+
+              </div>      
+            </div>
+               <div class="col-md-4">
+                <div class="about-box">
+                <img src="{{asset('site/image/6.jpg')}}" alt="" class="img-fluid">
+              </div>      
+            </div>
+              <div class="col-md-4">
+              <div class="about-box">
+              <img src="{{asset('site/image/15.jpg')}}" alt="" class="img-fluid">
+              </div>      
+            </div>
+              <div class="col-md-4">
+              <div class="about-box">
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deleniti voluptatum quae suscipit? Eligendi perferendis possimus, nisi molestiae, molestias neque veritatis quisquam sapiente sequi officiis quos esse, illum ducimus! Minima, aliquam.</p>
+              </div>      
+            </div>
+              <div class="col-md-4">
+              <div class="about-box">
+              <img src="{{asset('site/image/13.jpg')}}" alt="" class="img-fluid" style="height:210px;">
+           
+              </div>      
+            </div>  
+        </div>
+      </div>  
+</section>
+  <!-- about section end here -->
+     
+     <!-- service section starts here -->
+     <section id="service">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="section-title">
+              <h6>Services</h6>    
+              </div>
+            </div>             
+          </div>
+          <div class="row">
+            <div class="col-md-4">
+              <div class="service-box">
+              <i class="fa-solid fa-wheelchair-move icon"></i>
+                <h1>Wheelchair</h1>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                  quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                  <a href="#" class="btn">Read  <i class="fa-solid fa-book-open-reader"></i></a>
+              </div>      
+            </div>
+            <div class="col-md-4">
+              <div class="service-happy">
+              <i class="fa-solid fa-face-smile icon"></i>
+                <h1>Happy</h1>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                  quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                  <a href="#" class="btn">Read  <i class="fa-solid fa-book-open-reader"></i></a>
+              </div>      
+            </div>
+               <div class="col-md-4">
+                <div class="service-bus">
+                <i class="fa-solid fa-bus icon"></i>
+                <h1>Bus</h1>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                  quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                  <a href="#" class="btn">Read  <i class="fa-solid fa-book-open-reader"></i></a>
+              </div>      
+            </div>
+              <div class="col-md-4">
+              <div class="service-chair">
+              <i class="fa-solid fa-chair icon"></i>
+                <h1>Chair</h1>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                  quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                  <a href="#" class="btn">Read  <i class="fa-solid fa-book-open-reader"></i></a>
+              </div>      
+            </div>
+              <div class="col-md-4">
+              <div class="service-book ">
+              <i class="fa-solid fa-book icon"></i>
+                <h1>Education</h1>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                  quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                  <a href="#" class="btn">Read  <i class="fa-solid fa-book-open-reader"></i></a>
+              </div>      
+            </div>
+              <div class="col-md-4">
+              <div class="service-help">
+              <i class="fa-solid fa-handshake-angle icon"></i>
+               <h1>Help</h1>
+                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                  quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                  <a href="#" class="btn">Read  <i class="fa-solid fa-book-open-reader"></i></a>
+              </div>      
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </section>
+     <!-- our class section ends here -->
+
+  <!-- office -->
+  <section id="office">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="office-title">
+            <h1>Different Activity </h1>
+          </div>
+        </div>
+      </div>
+              <div class="row row-cols-1 row-cols-md-3 g-4">
+          <div class="col">
+            <div class="card">
+            <img src="{{asset('site/image/aq.jpg')}}" alt="" class="img-0fluid">
+              <div class="card-body">
+                <h5 class="card-title">Playing</h5>
+                <p class="card-text">Wheelchair for disabled rrfugee choldgren</p>
+              </div>
+            </div>
+          </div>
+          <div class="col">
+            <div class="card">
+            <img src="{{asset('site/image/as.jpg')}}" alt="" class="img-0fluid">
+              <div class="card-body">
+                <h5 class="card-title">Job</h5>
+                <p class="card-text">Disabled students studying at home on Wheelchair Stock.</p>
+              </div>
+            </div>
+          </div>
+          <div class="col">
+            <div class="card">
+            <img src="{{asset('site/image/ae.jpg')}}" alt="" class="img-0fluid">
+              <div class="card-body">
+                <h5 class="card-title">Studying</h5>
+                <p class="card-text">A socail service for familites with children with disabilities. </p>
+              </div>
+            </div>
+          </div>
+          
+          </div>
+        </div>
+              
+     </div>
+ </section>
+<!-- endoffice -->
+
+<!-- footer -->
+<section id="footer" style="background-image: linear-gradient(4deg,  #030eff, transparent); border-radius: 50px 35px 15px 15px"> 
+   <div class="container">
+    <div class="row">       
+      <div class="col-md-12">
+      <div class="text">
+        <h4>Indrouction  </h4>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum, totam saepe? Voluptate placeat quos necessitatibus explicabo? Accusamus tempora id, nobis vitae autem, explicabo asperiores et expedita ratione sapiente fuga nam.</p>
+        </div>
+      </div>
+      
+      <div class="col-md-3">
+        <div class="footer-links">
+          <h4>Quick Links  <i class="fa-solid fa-link"></i></h4>
+          <ul>
+            <li><a href="">HOME  <i class="fa-solid fa-house-user"></i></a></li>
+            <li><a href="">ABOUTS  <i class="fa-solid fa-address-card"></i></a></li>
+            <li><a href="">SERVICES  <i class="fa-solid fa-taxi"></i>    </a></li>
+            <li><a href="">CACEER  <i class="fa-solid fa-user-tie"></i></a></li>
+            <li><a href="">CONTACT  <i class="fa-solid fa-phone"></i></a></li>
+          </ul>
+        </div>
+       </div>
+
+         
+      <div class="col-md-3">
+        <div class="footer-contact  ">
+          <h4>Contact Us  <i class="fa-solid fa-address-book"></i></i></h4>
+          <ul>
+            <li><a href="">Phone +977-98468565858  <i class="fa-solid fa-phone"></i></a></li>
+            <li><a href="">Malepatan-7,Pokhara  <i class="fa-solid fa-address-card"></i></a></li>
+            <li><a href="">Mail:anishjirel123@gmail.com  <i class="fa-solid fa-message"></i></a></li>
+            <h2>or</h2>
+            <li><a href="">anishjirel7295@gmail.com  <i class="fa-solid fa-user-tie"></i></a></li>
+            
+          </ul>
+        </div>
+       </div>
+
+       <div class="col-md-3">
+        <div class="footer-links">
+          <h4>SOCIAL MEDIA    <i class="fa-brands fa-internet-explorer"></i></h4>
+          <ul>
+            <li><a href="">FACEBOOK   <i class="fa-brands fa-facebook"></i></a></li>
+            <li><a href="">YOUTUBE  <i class="fa-brands fa-youtube"></i></a></li>
+            <li><a href="">INSatagram   <i class="fa-brands fa-instagram"></i></a></li>
+            <li><a href="">VIBER CALL  <i class="fa-brands fa-viber"></i></a></li>
+           
+          </ul>
+        </div>
+       </div>
+
+    <div class="col-md-3">
+        <div class="footer-links">
+          <h4>Location Map <i class="fa-solid fa-map"></i></h4>
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7031.253479139802!2d83.96824842269123!3d28.218650930331393!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x399594ffac278acd%3A0xf35d1a23afd87cf4!2sMalepatan%2C%20Pokhara%2033700!5e0!3m2!1sen!2snp!4v1664016119840!5m2!1sen!2snp"></iframe>
+        </div>
+       </div>
+   </div>  
+</section>      
+<!-- footer section end here -->
+ <section id="last">
+  <div class="container">
+  <div class="col-md-12 text-right" style="color: black; letter-spacing:2px; font-weight:bolder;">
+        "@2001CopyrighDisabled.com"
+    </div>
+  </div>
+ </section> 
+<!-- footer section end here -->
+
+<body>
+<script src="{{asset('site/js/jquery.js')}}"></script>
+    <script src="{{asset('site/bootstrap/js/bootstrap.js')}}"></script>
+    <script src="{{asset('site/fontawesome/js/all.js')}}"></script>
+    <script src="{{asset('site/js/script.js') }}"></script>
+</body>
+</html>
